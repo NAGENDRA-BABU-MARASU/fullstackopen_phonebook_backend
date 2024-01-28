b69c1f8639e8fcb2b639410c22b098267ebc9f17
Nagendra <nagendrababu.marasu@gmail.com>
2024-01-28 12:46:27 +0530

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+
+const Person = require('./models/person');
 
 morgan.token('body', function getBody(req) {
 	return JSON.stringify(req.body);
@@ -43,63 +46,139 @@ app.get('/', (req, res) => {
 	res.send('Hello world');
 });
 
-app.get('/api/persons', (req, res) => {
-	res.json(persons);
+app.get('/api/persons', (req, res, next) => {
+	Person.find({})
+		.then((persons) => {
+			res.json(persons);
+		})
+		.catch((error) => next(error));
 });
 
-app.get('/api/persons/:id', (req, res) => {
-	const id = Number(req.params.id);
-	const person = persons.find((person) => person.id === id);
-	if (person) {
-		res.json(person);
-	} else {
-		res.status(404).end();
-	}
+app.get('/api/persons/:id', (req, res, next) => {
+	Person.findById(req.params.id)
+		.then((person) => {
+			if (person) {
+				res.json(person);
+			} else {
+				res.status(404).end();
+			}
+		})
+		.catch((error) => {
+			next(error);
+		});
 });
 
-const generateId = () => {
-	const max = 1000000;
-	return Math.floor(Math.random() * max);
-};
-
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
 	const body = req.body;
+
 	if (!body.name || !body.number) {
 		return res.status(400).json({
 			error: 'missing name or number',
 		});
 	}
-	const alreadyExists = persons.find(
-		(person) => person.name === body.name
-	);
-	if (alreadyExists) {
-		return res.status(400).json({
-			error: 'name must be unique',
+
+	Person.find({ name: body.name }).then((person) => {
+		if (person.length === 0) {
+			const person = new Person({
+				name: body.name,
+				number: body.number,
+			});
+
+			person
+				.save()
+				.then((savedPerson) => {
+					res.json(savedPerson);
+				})
+				.catch((error) => next(error));
+		} else {
+			return res.status(400).json({
+				error: 'name must be unique',
+			});
+		}
+	});
+
+	// const body = req.body;
+	// if (!body.name || !body.number) {
+	// 	return res.status(400).json({
+	// 		error: 'missing name or number',
+	// 	});
+	// }
+	// const alreadyExists = persons.find(
+	// 	(person) => person.name === body.name
+	// );
+	// if (alreadyExists) {
+	// 	return res.status(400).json({
+	// 		error: 'name must be unique',
+	// 	});
+	// } else {
+	// 	const person = {
+	// 		name: body.name,
+	// 		number: body.number,
+	// 		id: generateId(),
+	// 	};
+	// 	persons = persons.concat(person);
+	// 	res.json(person);
+	// }
+});
+
+app.delete('/api/persons/:id', (req, res, next) => {
+	Person.findByIdAndDelete(req.params.id)
+		.then((result) => {
+			res.status(204).end();
+		})
+		.catch((error) => next(error));
+});
+
+app.put('/api/persons/:id', (req, res, next) => {
+	const body = req.body;
+	const person = {
+		name: body.name,
+		number: body.number,
+	};
+
+	Person.findByIdAndUpdate(req.params.id, person, {
+		new: true,
+		runValidators: true, 
+		context: 'query'
+	})
+		.then((updatedPerson) => {
+			res.json(updatedPerson);
+		})
+		.catch((error) => next(error));
+});
+
+app.get('/info', (req, res, next) => {
+	Person.find({})
+		.then((persons) => {
+			res.send(
+				`Phonebook has info for ${
+					persons.length
+				} people <br/> ${new Date()}`
+			);
+		})
+		.catch((error) => next(error));
+});
+
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({
+		error: 'Unknown endpoint',
+	});
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+	console.log(error.message);
+
+	if (error.name === 'CastError') {
+		return res.status(400).send({
+			error: 'malformatted id',
 		});
-	} else {
-		const person = {
-			name: body.name,
-			number: body.number,
-			id: generateId(),
-		};
-		persons = persons.concat(person);
-		res.json(person);
+	} else if (error.name === 'ValidationError') {
+		return res.status(400).json({ error: error.message });
 	}
-});
-
-app.delete('/api/persons/:id', (req, res) => {
-	const id = Number(req.params.id);
-	persons = persons.filter((person) => person.id !== id);
-	res.status(204).end();
-});
-
-app.get('/info', (req, res) => {
-	res.send(
-		`Phonebook has info for ${
-			persons.length
-		} people <br/> ${new Date()}`
-	);
-});
+	next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
